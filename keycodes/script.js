6beb1bbe57f56c718a85e99501dd6dc442f11105
Omar <omerq779@gmail.com>
2023-08-08 12:28:05 +0400

    Key Codes CSS

--- keycodes/script.js
+++ keycodes/script.js
@@ -1,20 +1,27 @@
-let insert=document.getElementById("insert")
+let insert = document.getElementById("insert")
 
-window.addEventListener("keydown",(event)=>{
+window.addEventListener("keydown", (event) => {
 
-    console.log(event)
-    insert.innerHTML=`<div class="key">
-            ${event.key == " " ? "Space" :event.key}
+    //  console.log(event)
+    insert.innerHTML = `<div class="key">
+            ${event.key == " " ? "Space" : event.key}
+            
             <small>Event-Key</small>
 
         </div>
         <div class="key">
             ${event.keyCode}
-
+<br>
             <small>Event-KeyCode</small>
         </div>
         <div class="key">
             ${event.code}
             <small>event.code</small>
-        </div>`
+        </div>
+        
+        <div class="key key1">
+        key ve keycode almak icin herhangi bir tusa basin
+    </div>
+    `
+
 })
