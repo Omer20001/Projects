bb8ba1981fa0b4b0def728f7b60e641381932d77
Omar <omerq779@gmail.com>
2023-08-12 14:20:10 +0400

    mobile tab navigation

---
+++ mobile_tab_navigation/script.js
@@ -0,0 +1,25 @@
+const contents=document.querySelectorAll(".content")
+
+const navBtn=document.querySelectorAll("nav ul li")
+
+
+
+navBtn.forEach((btn,id    )=>{
+btn.addEventListener("click",()=>{
+    hideAllContents()
+    hideAllBtns()
+    btn.classList.add("active")
+    contents[id].classList.add("show")
+})
+})
+
+function     hideAllContents(){
+    contents.forEach((content)=>{
+        content.classList.remove('show')
+    })
+}
+function    hideAllBtns(){
+    navBtn.forEach((item)=>{
+        item.classList.remove('active')
+    })
+}
